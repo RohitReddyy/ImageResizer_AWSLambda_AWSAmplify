cf1e552acd246810daaa143458870d2ff1307caf
rmusukudabbidi <rmusukudabbidi@slu.edu>
2025-07-02 11:49:12 -0500

    Running Code

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "imageresize7bf1ce42": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Triggerb12b77ba": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "imageresizer8139044": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import {Amplify, Auth} from "aws-amplify";
+import awsconfig from './aws-exports';
 import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
